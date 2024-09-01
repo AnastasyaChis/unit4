8e5699807e777ff62fe41b74acd8a16adb5c8deb
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-09-01 22:15:33 +0300

    Correctoin error commit

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -21,15 +21,15 @@ public class AppCardDeliveryTest {
     public void shouldSuccessfullyTest(){
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Томск");
-        String planningData = generateData(4,"dd.mm.yyyy");
+        String planningData = generateData(4,"dd.MM.yyyy");
         $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.DELETE);
         $("[data-test-id= 'date'] input").setValue(planningData);
         $("[data-test-id= 'name'] input").setValue("Суворов Геннадий");
         $("[data-test-id= 'phone'] input").setValue("+79147364475");
         $("[data-test-id= 'agreement']").click();
         $("[class='button__content']").click();
-        $("[data-test-id= '.notification__content']")
-                .shouldBe(Condition.visible,Duration.ofSeconds(15))
+        $("[class='notification__content']")
+                .shouldBe(Condition.visible,Duration.ofSeconds(30))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningData));
 
 
